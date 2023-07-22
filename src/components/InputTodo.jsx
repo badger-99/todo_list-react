32d89be7a982e9c257bee32fa5d3977457c8ad4b
Alfred Peter <alpeter91@gmail.com>
2023-07-23 01:52:18 +0300

    Add task input functionality

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,6 +1,37 @@
-const InputTodo = () => {
+import { useState } from 'react';
+import { v4 } from 'uuid';
+
+const InputTodo = ({ tasks, setTasks }) => {
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) {
+      setMessage('Please add item');
+      return;
+    }
+    const newTask = {
+      id: v4(),
+      title: value,
+      complete: false,
+    };
+    setTasks([...tasks, newTask]);
+    setValue('');
+    setMessage('');
+  };
   return (
-    <div>Input field goes here...</div>
-  )
-}
-export default InputTodo
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type='submit'>submit</button>
+      </form>
+      <span>{message}</span>
+    </>
+  );
+};
+export default InputTodo;
