c3695983e7a65bfa8d2e66bf2bf58964efd55647
Alfred Peter <alpeter91@gmail.com>
2023-07-22 23:43:21 +0300

    Add contextAPI

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,12 +1,22 @@
+import { useState, createContext, useContext } from "react";
 import InputTodo from "./InputTodo";
 import TodoList from "./TodoList";
+import todos from './todos';
+
+const items = todos;
+export const TodoContext = createContext()
+export const todoAppContext = useContext(TodoContext)
 
 const TodosLogic = () => {
+  const [tasks, setTasks] = useState(items);
+
   return (
+    <TodoContext.Provider value={{tasks, setTasks}}>
     <div>
       <InputTodo />
       <TodoList />
     </div>
+    </TodoContext.Provider>
   )
 }
 export default TodosLogic
