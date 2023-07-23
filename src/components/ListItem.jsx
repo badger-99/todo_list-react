ddb7167ebee9e421a82423e3594a9fcfb3b77cab
Alfred Peter <alpeter91@gmail.com>
2023-07-23 18:20:51 +0300

    Add list item elements

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from '@/styles/TodoItem.module.css';
 
 const ListItem = ({ task, handleClick, deleteTask }) => {
@@ -9,19 +10,42 @@ const ListItem = ({ task, handleClick, deleteTask }) => {
     opacity: 0.4,
     textDecoration: 'line-through',
   };
+
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
   return (
     <li className={styles.item}>
       <div className={styles.content} style={viewMode}>
+        <input
+          type='checkbox'
+          checked={completed}
+          id={id}
+          onChange={() => handleClick(id)}
+        />
+        <span style={completed ? completedStyle : null}>{title}</span>
+        <button type='button' onClick={() => deleteTask(id)}>
+          Remove
+        </button>
+        <button onClick={handleEditing}>Edit</button>
+      </div>
       <input
         type='text'
         value={title}
-        readOnly={isEditable}
-        style={{ width: '13rem', border: 'none' }}
+        className={styles.textInput}
+        style={editMode}
       />
-      <button type='button' onClick={() => deleteTask(id)}>
-        remove
-      </button>
-      <button type='button' onClick={()=>setIsEditable(!isEditable)}>edit</button>
     </li>
   );
 };
