271626cad5cc3fcbaff31f427e8a3814af06fa11
Alfred Peter <alpeter91@gmail.com>
2023-07-23 19:12:13 +0300

    Add local storage functionality

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import InputTodo from './InputTodo';
 import TodoList from './TodoList';
 import todos from './todos';
 
 
 const TodosLogic = () => {
-  const [tasks, setTasks] = useState(todos);
+  const getInitialTodos = () => {
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
+
+  const [tasks, setTasks] = useState(getInitialTodos());
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(tasks);
+    localStorage.setItem('todos', temp);
+  }, [tasks]);
 
   return (
     <div>
